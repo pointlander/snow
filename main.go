3accff08c5f9fdddeceb485ae03ba319f3e3e9b8
Andrew Snodgrass <pointlander@gmail.com>
2025-02-02 22:02:29 -0600

    Added feedback

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 
 	"github.com/alixaxel/pagerank"
@@ -18,16 +19,33 @@ func main() {
 		u.Data = append(u.Data, rng.Float32())
 	}
 
-	graph := pagerank.NewGraph()
-	for i := 0; i < u.Rows; i++ {
-		for j := 0; j < u.Rows; j++ {
-			p := CS(u.Data[i*u.Cols:(i+1)*u.Cols], u.Data[j*u.Cols:(j+1)*u.Cols])
-			graph.Link(uint32(i), uint32(j), float64(p))
+	for {
+		graph := pagerank.NewGraph()
+		for i := 0; i < u.Rows; i++ {
+			for j := 0; j < u.Rows; j++ {
+				p := CS(u.Data[i*u.Cols:(i+1)*u.Cols], u.Data[j*u.Cols:(j+1)*u.Cols])
+				graph.Link(uint32(i), uint32(j), float64(p))
+			}
 		}
+		ranks := make([]float64, u.Rows)
+		graph.Rank(1.0, 1e-3, func(node uint32, rank float64) {
+			ranks[node] = rank
+		})
+		min, index := math.MaxFloat64, 0
+		for i, v := range ranks {
+			if v < min {
+				min, index = v, i
+			}
+		}
+		for i, v := range ranks {
+			u.Data[index*u.Cols+i] = float32(v)
+		}
+		for i := 0; i < u.Rows; i++ {
+			for j := 0; j < u.Cols; j++ {
+				fmt.Printf("%f ", u.Data[i*u.Cols+j])
+			}
+			fmt.Println()
+		}
+		fmt.Println()
 	}
-	ranks := make([]float64, u.Rows)
-	graph.Rank(1.0, 1e-3, func(node uint32, rank float64) {
-		ranks[node] = rank
-	})
-	fmt.Println(ranks)
 }
